b7ca414c46e722ae3fe59ee860e9b82438880787
Roman Karpov <roman.karpov@devexpress.com>
2019-06-11 14:25:45 +0300

    Commit the example.

---
+++ CS/GettingStarted2/Views/NavPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GettingStarted2.Views {
+    public partial class NavPage : NavigationPage {
+        public NavPage() {
+            InitializeComponent();
+        }
+
+        public NavPage(Page mainPage): base(mainPage) {
+            InitializeComponent();
+        }
+    }
+}
